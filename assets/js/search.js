119159a253f68e9e4bf0e435c06c2b01e30f4902
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-07-16 01:00:28 -0400

    Added styling to the pages, added search results as renderd cards to the search page and added an enter key event listener so now inputs can be reegesterd after pressing the enter key

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,63 +1,104 @@
+var resultsEl = document.getElementById("results");
+
+// During production and testing, replace with your own key please :)
+// Made it global so it can be accessed by the
+const API_KEY = "57046b00";
 
 // Call OMDb API to get movie data for a search term
 async function searchMovie(searchTerm) {
     
-    // During production and testing, replace with your own key please :)
-    const API_KEY = "f3ecd794";
+  // Create a search request URI with a given search term
+  const QUERY_URI = `https://www.omdbapi.com/?s=${searchTerm}&apikey=${API_KEY}`;
 
-    // Create a search request URI with a given search term
-    const QUERY_URI = `https://www.omdbapi.com/?s=${searchTerm}&apikey=${API_KEY}`;
-
-    // Attempt to fetch data asynchronously ("await") for the searchTerm
-    var result = await fetch(QUERY_URI)
-
+  // Attempt to fetch data asynchronously ("await") for the searchTerm
+  var result = await fetch(QUERY_URI)
     // Standard getting json from response
-    .then(response => response.json())
+    .then((response) => response.json())
 
     // Return the actual data we care about (stored in result variable)
-    .then(data => {
-        return data;
+    .then((data) => {
+      return data;
     })
 
     // Log any errors that occur
-    .catch(error => console.log(error));
+    .catch((error) => console.log(error));
 
-    // Return final result array
-    return result.Search;
-
+  // Return final result array
+  return result.Search;
 }
 
 async function getSearchedMovie() {
+  // Get search term from URL parameters
+  var url = new URL(window.location.href);
+  var searchTerm = url.searchParams.get("q");
 
-    // Get search term from URL parameters
-    var url = new URL(window.location.href);
-    var searchTerm = url.searchParams.get("q");
-
-    // Return the result of calling the searchMovie function with the searchTerm
-    return await searchMovie(searchTerm);
+  // Return the result of calling the searchMovie function with the searchTerm
+  return await searchMovie(searchTerm);
 }
 
 // Call all set up functions inside here - event handlers, element creation, other page setup
 // Must be asynchronous in order to use await keyword for awaiting API response
 async function init() {
+  // Get results for the searched term
+  var results = await getSearchedMovie();
 
-    // Get results for the searched term
-    var results = await getSearchedMovie();
+  // Adds a random "movies" background
+  document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?movie')`;
 
-    // Just to show that the results do appear successfully
-    console.log(results);
-
-    // Loop over each result to render them
-    results.forEach(result => {
-
-        // Create a new elemt with jQuery and set its text using ES6 String Templates
-        var resultEl = $("<h6/>").text(`Title: ${result.Title}, year: ${result.Year}`);
-        
-        // Append each resulting element to a div in the html
-        $("#results").append(resultEl);
-    });
-
+  // Calls the getPostersInfo function and pass the results
+  getPostersInfo(results);
 }
+
+// This function will make an api call for each title resulted in the original search
+var getPostersInfo = function (results) {
+  // Loops through the results and return more information about each returned title with a max of 8
+  for (let i = 0; i < results.length && i < 8; i++) {
+    // Create a search request URI for each returned title
+    var requestUrl = `https://www.omdbapi.com/?t=${results[i].Title}&apikey=${API_KEY}`;
+
+    // Makes an api call for the given title
+    fetch(requestUrl)
+      // Gets a JSON object containing the data needed
+      .then((response) => response.json())
+
+      // Once gets the data calls renderPosterCards
+      .then((data) => renderPosterCards(data))
+      // Log any errors that occur
+      .catch((error) => console.log(error));
+  }
+};
+
+// This function will render a card for each movie title returned
+var renderPosterCards = function (data) {
+  // Checks if there is an error with any of the movie objects returned then skip that movie from the render
+  if (!data.Error) {
+    // Injects a card for each movie title with it's title, poster, year, actors etc
+    resultsEl.innerHTML += `
+       <div class="col s12 m3">
+        <div class="card large">
+        <div class="card-image waves-effect waves-block waves-light">
+          <img class="activator" src="${data.Poster}">
+        </div>
+
+        <div class="card-content">
+          <span class="card-title activator grey-text text-darken-4">${data.Title}<br/>
+           ${data.Year}
+           <i class="material-icons tiny">fiber_manual_record</i>
+           ${data.Runtime}
+            <i class="material-icons right">arrow_upward</i></span>
+        </div>
+
+        <div class="card-reveal">
+          <span class="card-title grey-text text-darken-4">${data.Title}  <i class="material-icons right">close</i></span>
+          <p>Genre: ${data.Genre}</p><br />
+          <p>${data.Plot}</p><br />
+          <p>Actors: ${data.Actors}</p><br />
+          <p>IMDB Rating: ${data.imdbRating}</p>
+        </div>
+      </div>
+      </div>`;
+  }
+};
 
 // Single function call to set up webpage
 init();
