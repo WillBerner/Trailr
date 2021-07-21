e18b679626f0b1ca32cf2ca607f7ca8d37a044b4
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-07-20 21:18:15 -0400

    Added the basic parallax materialize.css structure with the java script needed for it to work

---
+++ assets/js/about.js
@@ -0,0 +1,11 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, 0);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, 200);
+  }); 
