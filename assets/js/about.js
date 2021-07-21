3a1b80bdfdbcc3e5475056923ad0ab7ae3588bcd
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-07-20 23:31:16 -0400

    Added comments to the javaScript parallax function

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,7 +1,8 @@
-
-
+// This function will add an event listener to the Dom content that is loaded 
 document.addEventListener('DOMContentLoaded', function() {
+  // Will assign the elems to all div with the class of parallax
     var elems = document.querySelectorAll('.parallax');
+    // Will initiate the parallax
     var instances = M.Parallax.init(elems, 0);
   });
 
