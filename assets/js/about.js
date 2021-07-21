739d73beb6a9cb6227bbb077e6e688a6fde9fdb6
Muiasar-Al-Ani <82427073+Muiasar-Al-Ani@users.noreply.github.com>
2021-07-20 23:00:05 -0400

    Added personal pictures and cards with personal statement

--- assets/js/about.js
+++ assets/js/about.js
@@ -5,7 +5,3 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Parallax.init(elems, 0);
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, 200);
-  }); 
