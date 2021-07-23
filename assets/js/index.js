dd72757b96e6cab3db8798697fac94538a78199a
Will Berner <will.o.berner@gmail.com>
2021-07-23 04:18:49 -0400

    Heavily refactored JS files for organization. Implemented responsive filtering by year.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,28 @@
-// the movie data base key
+// TheMovieDataBase key
 const TMDB_API_KEY = '35bedaf996a0d463f1f8fa5911ed61f8'
 
-// Calls TMDb API to get top rated movies list
-function getTopRated() {
-
-  // Fetch request gets top rated movies
-  var topMoviesRequest = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_video=false&page=1`
-
-  // Makes an api call to the TMDB database
-  fetch(topMoviesRequest)
-    // Standard getting json from response
-    .then((response) => response.json())
-
-    // Return the actual data we care about (stored in result variable)
-    .then((data) => {
-      // Calls the renderTopRated and passing in the data retrieved 
-      renderTopRated(data)
-    })
-}
-
-// Get any previous movie search terms the user has already used
-// Nonfunctional currently, for future use.
-function loadPreviousSearches() {
-
-  // Try to get previous search terms from local storage
-  var previousSearchTerms = JSON.parse(localStorage.getItem("prevSearches"));
-
-  // If there exists no terms yet (new user), create new array
-  if (!previousSearchTerms) {
-    
-    // Create empty array
-    previousSearchTerms = [];
-
-    // Save empty array to local storage
-    localStorage.setItem("prevSearches", JSON.stringify(previousSearchTerms));
-  }
-
-  // Return array of search terms (possibly empty)
-  return previousSearchTerms;
-}
+// Single function call to set up webpage
+init();
 
 // Call all set up functions inside here - event handlers, element creation, other page setup
 function init() {
+
+  // Check if user was redirected after no search results were found
+  checkForNoResults();
+
+  // Adds a random "movie" background
+  document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?movie')`;
+
+  // Get and display new, top rated movies
+  getTopRated();
+
+  // Set up event handlers for the page
+  setupEventHandlers();
+
+}
+
+// This function checks if a user just searched for a movie that returned no results
+function checkForNoResults() {
 
   // Get not found key-word from the URL 
   var url = new URL(window.location.href);
@@ -49,49 +30,110 @@ function init() {
 
   // Checks if the URL ends with not found key word
   if (notFound === 'not-found') {
-    
+
     // Replaces the input placeholder with this warning and prompts for entering another title 
-    document.getElementById("searchInput").placeholder = 'There was no movie with this title - please try again!'
+    document.getElementById("searchInput").placeholder = "We couldn't find a movie with that title - please try again!"
   }
-
-  // Get previous search terms (nonfunctional currently, for future use)
-  var prevSearches = loadPreviousSearches();
-
-  // Adds a random "movies" background
-  document.body.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?movie')`;
-
-  getTopRated();
-
-  // Adds the event handler for the search button
-  document.getElementById("searchButton").addEventListener("click", searchBarHandler);
-
-  // Listens to an 'Enter' key for the form to accept input on the keyup
-  document.getElementById("searchInput").addEventListener("keyup", enterKeyHandler);
-
-   // Adds event listener for clicking on a movie card view button
-  document.getElementById("top-rated").addEventListener('click', viewButtonClickHandler);
-
 }
 
-// Save a search term to localstorage
+// This function saves a search term to localstorage
 function saveSearchTerm(newSearchTerm) {
 
-  // Get previous search terms array
-  var previousSearchTerms = JSON.parse(localStorage.getItem("prevSearches"));
-
-  // Append the new search term to be saved to the array
-  previousSearchTerms.push(newSearchTerm);
-
   // Save the search term array back to local storage
-  localStorage.setItem("prevSearches", JSON.stringify(previousSearchTerms));
+  localStorage.setItem("prevSearchTerm", newSearchTerm);
 
 }
 
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////// API QUERY FUNCTIONS /////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+// This function makes an API call to the TMDb API to get a list of top rated movies
+function getTopRated() {
+
+  // Fetch request gets top rated movies
+  var topMoviesRequest = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_video=false&page=1`
+
+  // Makes an api call to the TMDB database
+  fetch(topMoviesRequest)
+
+    // Standard getting json from response
+    .then((response) => response.json())
+
+    // Return the actual data we care about (stored in result variable)
+    .then((data) => {
+
+      // Calls the renderTopRated and passing in the data retrieved 
+      renderTopRated(data)
+    })
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////// EVENT HANDLER FUNCTIONS /////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+// This function sets up event handlers for the page
+function setupEventHandlers() {
+
+  // Adds event handler for the search button
+  document.getElementById("searchButton").addEventListener("click", searchBarHandler);
+
+  // Adds event handler for an 'Enter' key on the search bar
+  document.getElementById("searchInput").addEventListener("keyup", enterKeyHandler);
+
+  // Adds event handler for clicking on a movie card "view" button
+  document.getElementById("top-rated").addEventListener('click', handleTrailerLink);
+
+}
+
+// Event handler for entering a search request 
+function searchBarHandler() {
+
+  // Get the current value of the search text-input
+  var searchTerm = document.getElementById("searchInput").value;
+
+  // If the user didn't type in a value, display an error and don't redirect
+  if (!searchTerm) {
+    document.getElementById("searchInput").placeholder = "Error! Please enter a movie to search for.";
+    return;
+  }
+
+  // Save the search term to local storage if it isn't blank
+  saveSearchTerm(searchTerm);
+
+  // redirect the user to the new page with a parameter of the search term
+  window.location = `./search.html?q=${searchTerm}`;
+
+};
+
+// Event handler for when a user clicks "view" on a movie card
+function handleTrailerLink(event) {
+
+  // Get title of movie from its html data-attribute
+  var movieTitle = event.target.getAttribute('data-title');
+
+  // If the user actually clicked on a the view button, redirect to it.
+  if (movieTitle) {
+    window.location = `./trailer.html?q=${movieTitle}`
+  }
+}
+
+// Event handler for entering a search request on the enter key
+function enterKeyHandler(event) {
+  if (event.key === "Enter") {
+    searchBarHandler();
+  }
+};
+
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////// RENDERING FUNCTIONS /////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
 // The renderTopRated function renders a card for each of the topRated movies
-function renderTopRated(data){
+function renderTopRated(data) {
   // Gets the results array from the data and store it in a variable
   var topMovies = data.results;
-  
+
   for (var i = 0; i < topMovies.length; i++) {
     // Injects a card for each movie title with it's title, poster, release date etc
     document.getElementById("top-rated").innerHTML += `
@@ -110,57 +152,6 @@ function renderTopRated(data){
         </div>
       </div>
     </div>`;
-    
+
   }
 }
-
-// Handle a search request 
-function searchBarHandler() {
-
-  // Get the current value of the search text-input
-  var searchTerm = document.getElementById("searchInput").value;
-
-  // Save the search term to local storage
-  saveSearchTerm(searchTerm);
-
-  // If the user didn't type in a value, display an error and don't redirect
-  if (!searchTerm) {
-    document.getElementById("searchInput").placeholder = "Error! Please enter a movie to search for.";
-    return;
-  }
-
-  // redirect the user to the new page with a parameter of the search term
-  window.location = `./search.html?q=${searchTerm}`;
-
-};
-
-// This function directs the user to a new page with a trailer for the movie
-function viewButtonClickHandler(event){
-  // Get title of movie from its html data-attribute
-  var movieTitle = event.target.getAttribute('data-title');
-
-  // If the user actually clicked on a the view button, redirect to it.
-  if (movieTitle) {
-    window.location = `./trailer.html?q=${movieTitle}`
-  }
-}
-
-// This handler will check if the key pressed was an enter key to accept the input
-function enterKeyHandler(event) {
-  if (event.key === "Enter") {
-    searchBarHandler();
-  }
-};
-
-// Single function call to set up webpage
-init();
-
-
-
-
-
-
-
-
-
-
--- assets/js/trailer.js
+++ assets/js/trailer.js
@@ -3,7 +3,41 @@ const STREAMING_INFO_KEY = "daa11a7f40msh6e1ece24c7b095dp1df636jsn01c0d63b2684";
 const YOUTUBE_API_KEY = "AIzaSyDfqFtp0qlTg8E5PQqIj7nGkMOupJ5ZyD0";
 const OMBD_API_KEY = "f3ecd794";
 
-// Call YouTube API to get the movie trailer Id for a movie title
+// Start her up!
+init();
+
+// Starts up the page - sets up event handlers and renders movie info
+async function init() {
+
+  // Get results for the movie title
+  await getSearchedTrailer();
+
+  // Set up event handlers for the page
+  setupEventHandlers();
+}
+
+// Attempt to get data from the URI and call APIs based on info
+async function getSearchedTrailer() {
+
+  // Get movie title from URL parameters
+  var url = new URL(window.location.href);
+  var movieTitle = url.searchParams.get("q");
+
+  // If a user tries to search without a query string, return them to the home page.
+  if (!movieTitle) {
+    window.location = "./index.html";
+  }
+
+  // Calls the getPostersInfo and searchTrailer functions and pass in the movieTitle as parameter
+  getPosterInfo(movieTitle);
+  return await searchTrailer(movieTitle);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////// API QUERY FUNCTIONS /////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+// This function makes an API call to YouTube to get the movie trailer ID for a movie title
 async function searchTrailer(movieTitle) {
 
   // Create a search request URI with a given movie title
@@ -26,7 +60,34 @@ async function searchTrailer(movieTitle) {
   renderVideoPlayer(result.items[0].id.videoId);
 }
 
-// This function will make an api call and fetches the movie info and calling the renderMovieInfo and passing in the data
+// This function makes an API call to detemine the streaming availability of the movie
+// using it's IMDB ID and renders an IMDB link and possibly a netflix link to the page
+function servicesLinkGetter(data) {
+
+  // Create a search request URI for the given imdbID
+  var requestUrl = `https://streaming-availability.p.rapidapi.com/get/basic?country=us&imdb_id=${data.imdbID}`;
+
+  // Make an api call with given URL
+  fetch(requestUrl, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": STREAMING_INFO_KEY,
+      "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
+    },
+  })
+    // Gets a JSON object containing the data needed
+    .then((response) => response.json())
+
+    // Once gets the data calls makeLinks function and passing in the data
+    .then((data) => renderLinks(data))
+
+    //  Log any errors that occur
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+// This function makes an API call to fetch the movie info and then calls functions to render it
 function getPosterInfo(movieTitle) {
 
   // Create a search request URI for the given movieTitle
@@ -47,73 +108,62 @@ function getPosterInfo(movieTitle) {
     .catch((error) => console.log(error));
 }
 
-// This function will make an Api call to the streaming info api using the imdbID from the OMDB api
-function servicesLinkGetter(data) {
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////// RENDERING FUNCTIONS /////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 
-  // Create a search request URI for the given imdbID
-  var requestUrl = `https://streaming-availability.p.rapidapi.com/get/basic?country=us&imdb_id=${data.imdbID}`;
+// Function to create links to the IMDB page of the searched movie as well as to netflix.
+function renderLinks(data) {
 
-  // Make an api call with given URL
-  fetch(requestUrl, {
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": STREAMING_INFO_KEY,
-      "x-rapidapi-host": "streaming-availability.p.rapidapi.com",
-    },
-  })
-    // Gets a JSON object containing the data needed
-    .then((response) => response.json())
+  // Sets imdb button to be visible
+  document.getElementById("imdb").style.display = "inline-block";
 
-    // Once gets the data calls makeLinks function and passing in the data
-    .then((data) => makeLinks(data))
+  // Sets the data-set attribute to be the imdb link for the given movie
+  document.getElementById("imdb").setAttribute(
+    "data-service",
+    `https://www.imdb.com/title/${data.imdbID}`
+  );
 
-    //  Log any errors that occur
-    .catch((error) => {
-      console.error(error);
-    });
-}
+  // Checks if this path exist that means the movie is available in the netflix database
+  if (data.streamingInfo.netflix) {
 
-// Attempt to get data from the URI and call APIs based on info
-async function getSearchedTrailer() {
+    // if exists set netflix button to be visible
+    document.getElementById("netflix").style.display = "inline-block";
 
-  // Get movie title from URL parameters
-  var url = new URL(window.location.href);
-  var movieTitle = url.searchParams.get("q");
-
-  // If a user tries to search without a query string, return them to the home page.
-  if (!movieTitle) {
-    window.location = "./index.html";
+    // Sets the data-set attribute to be the netflix link for the given movie
+    document.getElementById("netflix").setAttribute(
+      "data-service",
+      data.streamingInfo.netflix.us.link
+    );
   }
-
-  // Calls the getPostersInfo and searchTrailer functions and pass in the movieTitle as parameter
-  getPosterInfo(movieTitle);
-  return await searchTrailer(movieTitle);
 }
 
-// Starts up the page - sets up event handlers and renders movie info
-async function init() {
-
-  // Get results for the movie title
-  var movieTitle = await getSearchedTrailer();
-
-  // initiate an event listener for the buttons in the streaming services div
-  document.getElementById("streaming-services").addEventListener("click", clickServicesHandler);
-
-  // Set up event handler for going back to the results page
-  setupBackButton();
+// Function for rendering extra, less important info
+function renderMovieExtraInfo(data) {
+  document.getElementById("info-rating").textContent = `Rating: ${data.imdbRating}`;
+  document.getElementById("info-genre").textContent = `Genre: ${data.Genre}`;
+  document.getElementById("info-director").textContent = `Director(s): ${data.Director}`;
+  document.getElementById("info-writer").textContent = `Writer(s): ${data.Writer}`;
+  document.getElementById("info-language").textContent = `Language: ${data.Language}`;
+  document.getElementById("info-released").textContent = `Release Date: ${data.Released}`;
 }
 
-// Function to return
-function setupBackButton() {
+// The renderMovieInfo will dynamically add the gathered data from the Api call in the appropriate divs
+function renderMovie(data) {
 
-  // Grab element
-  var backButton = document.getElementById("backToResultsButton");
+  // Display a color on the IMDB rating based on the score
+  renderRatingColor(data.imdbRating);
 
-  // Add event listener to redirect to previous page
-  backButton.addEventListener("click", (event) => {
-    window.location = document.referrer;
-  })
+  // Render extra, less important movie info
+  renderMovieExtraInfo(data);
 
+  // Populate html elements with movie data
+  document.getElementById("poster").src = data.Poster;
+  document.getElementById("title").textContent = data.Title;
+  document.getElementById("rating").textContent = `${data.imdbRating}`
+  document.getElementById("movie-info").textContent = `${data.Year} / ${data.Runtime} / ${data.Rated} / ${data.Genre}`;
+  document.getElementById("movie-actors").innerHTML = `${data.Actors}`;
+  document.getElementById("movie-summary").innerHTML = data.Plot;
 }
 
 // Embeds the trailer video to the page
@@ -129,7 +179,7 @@ function renderVideoPlayer(videoId) {
 }
 
 // Set the color of the score of the movie based on its value
-function setRatingColor(score) {
+function renderRatingColor(score) {
 
   // If an IMDB score doesn't exist (yet to be released movies, or movies with no rating)
   if (score == "N/A") {
@@ -150,63 +200,22 @@ function setRatingColor(score) {
   }
 }
 
-// The renderMovieInfo will dynamically add the gathered data from the Api call in the appropriate divs
-function renderMovie(data) {
-  
-  // Display a color on the IMDB rating based on the score
-  setRatingColor(data.imdbRating);
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////// EVENT HANDLER FUNCTIONS /////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 
-  // Render extra, less important movie info
-  renderMovieExtraInfo(data);
+// This function sets up event handlers for the page
+function setupEventHandlers() {
 
-  // Populate html elements with movie data
-  document.getElementById("poster").src = data.Poster;
-  document.getElementById("title").textContent = data.Title;
-  document.getElementById("rating").textContent = `${data.imdbRating}`
-  document.getElementById("movie-info").textContent = `${data.Year} / ${data.Runtime} / ${data.Rated} / ${data.Genre}`;
-  document.getElementById("movie-actors").innerHTML = `${data.Actors}`;
-  document.getElementById("movie-summary").innerHTML = data.Plot;
+  // initiate an event listener for the buttons in the streaming services div
+  document.getElementById("streaming-services").addEventListener("click", handleServices);
+
+  // Add event listener to redirect to previous page
+  document.getElementById("backToResultsButton").addEventListener("click", handleBackButton);
 }
 
-// Function for rendering extra, less important info
-function renderMovieExtraInfo(data) {
-  document.getElementById("info-rating").textContent = `Rating: ${data.imdbRating}`;
-  document.getElementById("info-genre").textContent = `Genre: ${data.Genre}`;
-  document.getElementById("info-director").textContent = `Director(s): ${data.Director}`;
-  document.getElementById("info-writer").textContent = `Writer(s): ${data.Writer}`;
-  document.getElementById("info-language").textContent = `Language: ${data.Language}`;
-  document.getElementById("info-released").textContent = `Release Date: ${data.Released}`;
-}
-
-// Function to create links to the IMDB page of the searched movie
-// as well as to netflix.
-function makeLinks(data) {
-
-  // Sets imdb button to be visible
-  document.getElementById("imdb").style.display = "inline-block";
-
-  // Sets the data-set attribute to be the imdb link for the given movie
-  document.getElementById("imdb").setAttribute(
-    "data-service",
-    `https://www.imdb.com/title/${data.imdbID}`
-  );
-
-  // Checks if this path exist that means the movie is available in the netflix database
-  if (data.streamingInfo.netflix) {
-
-    // if exists set netflix button to be visible
-    document.getElementById("netflix").style.display = "inline-block";
-    
-    // Sets the data-set attribute to be the netflix link for the given movie
-    document.getElementById("netflix").setAttribute(
-      "data-service",
-      data.streamingInfo.netflix.us.link
-    );
-  }
-}
-
-// Handler for clicking on the imdb or netflix links
-function clickServicesHandler(event) {
+// Event handler for clicking on the imdb or netflix links
+function handleServices(event) {
 
   // Gets the link from the clicked button
   var service = event.target.getAttribute("data-service");
@@ -230,5 +239,13 @@ function clickServicesHandler(event) {
   }
 }
 
-// Start her up!
-init();
+// Event listener to redirect the user to the previous search page
+function handleBackButton() {
+
+  // Get the most recently searched term
+  var lastSearchedTerm = localStorage.getItem("prevSearchTerm");
+
+  // Reload the search page with that term
+  window.location = `search.html?q=${lastSearchedTerm}`;
+
+}
